6f8c20738e3241abed4556bf1bfb0ac6dba7f88c
brusseau25 <brusseau27@gmail.com>
2016-03-15 10:40:23 -0400

    finished bbs method

--- ValueOfPi/src/PiDriver.java
+++ ValueOfPi/src/PiDriver.java
@@ -9,9 +9,11 @@
 
 public class PiDriver extends Pi {
 	public static void main(String[] args) {
-		int[] randIntArray = new int[5000];
+		int[] randIntArray = new int[10000];
 		int[] trngNum = new int[5000];
+		long[] bbsArray = new long[10000];
 		double piAverageTRNG = 0.0;
+		double piAvgBBS = 0.0;
 		double piAvgInt = 0.0;
 		int size = 15;
 
@@ -24,12 +26,20 @@ public class PiDriver extends Pi {
 		System.out.printf("Estimate of pi (trng array) = %f \n", piAverageTRNG);
 
 		for (int i = 1; i < (size + 1); i++) {
+			bbsArray = generateBBS(bbsArray);
+			calcGCD(bbsArray);
+			piAvgBBS += estimatePI(bbsArray);
+		}
+		piAvgBBS /= size;
+		System.out.printf("Estimate of pi (bbs array) = %f \n", piAvgBBS);
+
+		for (int i = 1; i < (size + 1); i++) {
 			randIntArray = initIntArray(randIntArray.length);
 			calcGCD(randIntArray);
 			piAvgInt += estimatePI(randIntArray);
 		}
 		piAvgInt /= size;
-		System.out.printf("Estimate of pi (int array) = %f \n", piAvgInt);
+		System.out.printf("Estimate of pi (lcg array) = %f \n", piAvgInt);
 
 	}
 }
