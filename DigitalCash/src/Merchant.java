bd347e548ca3e30da7f6300049f353f8be2edd71
brusseau25 <brusseau27@gmail.com>
2016-03-30 09:42:51 -0400

    added 2 paging algorithms

--- DigitalCash/src/Merchant.java
+++ DigitalCash/src/Merchant.java
@@ -1,4 +1,7 @@
 
 public class Merchant {
 
+	// verify bank signature
+
+	// randomly determine which half of identity string is revealed
 }
--- Paging/src/Driver.java
+++ Paging/src/Driver.java
@@ -1,5 +1,3 @@
-import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Driver {
@@ -7,18 +5,9 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = null;
-		PagingAlgorithms p = null;
-		int[] refString = null;
-		int frameNum = 0;
+		fifo p = new fifo();
 
-		try {
-			scan = new Scanner(new File("pages.dat"));
-			frameNum = scan.nextInt();
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		p.fifoAlgo();
 
 	}
 
